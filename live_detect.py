a921e2d4e5c9ec3cdbbb5e89904869efc169cee0
Mayookh M <79088427+mayookhm@users.noreply.github.com>
2022-11-13 14:47:37 +0530

    Add files via upload

---
+++ live_detect.py
@@ -0,0 +1,28 @@
+# import the opencv library
+import cv2
+body_classifier=cv2.CascadeClassifier('haarcascade_fullbody.xml')
+# Define a video capture object
+vid = cv2.VideoCapture(0)
+
+while(True):
+   
+    # Capture the video frame by frame
+    ret, frame = vid.read()
+    gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bodies = body_classifier.detectMultiScale(gray,1.1,4)
+
+    for (x,y,w,h) in bodies:
+       cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+    # Display the resulting frame
+    cv2.imshow("frame", frame)
+      
+    # Quit Window by Spacebar Key
+    if cv2.waitKey(25) == 32:
+        break
+  
+# After the loop release the cap object
+vid.release()
+
+# Destroy all the windows
+cv2.destroyAllWindows()
